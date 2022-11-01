466c57e877271feebeb1e1a2b5936ef4be31e81a
Klim Krivoguzov <krivoguzov.klim@yandex.ru>
2022-11-01 18:38:37 +0300

    Initial Commit

---
+++ RandomColors/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColors
+//
+//  Created by Klim Krivoguzov on 01.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
